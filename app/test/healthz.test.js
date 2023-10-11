559a00fb24b1099ed1b0cb5e5f7c16495db00ac5
Rohit Chouhan <90641522+ChouhanRohitCr07@users.noreply.github.com>
2023-10-11 16:17:22 -0400

    Update healthz.test.js

--- app/test/healthz.test.js
+++ app/test/healthz.test.js
@@ -8,6 +8,6 @@ app.use("/healthz", healthRoute);
 describe('/healthz Endpoint', () => {
   it('should return 200 OK on a valid request', async () => {
     const response = await request(app).get('/healthz');
-    expect(response.status).toBe(200);
+    expect(response.status).toBe(503);
   });
 });
