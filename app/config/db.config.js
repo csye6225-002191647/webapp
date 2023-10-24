5db7b7850a3048de21ddcbaba03b86ad931886e4
Rohit Chouhan <90641522+ChouhanRohitCr07@users.noreply.github.com>
2023-10-24 09:35:37 -0400

    config changes

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -2,13 +2,14 @@ const {Sequelize} = require('sequelize');
 const dbConfig = require('../config/app.config')
 
 const sequelize = new Sequelize({
-    HOST: dbConfig.HOSTNAME,
-    USER: dbConfig.USER,
-    DB: dbConfig.DB,
-    dialect: "postgres",
-    port: 5432,
-    PASSWORD: dbConfig.PASSWORD
-  })
+  host: dbConfig.HOST,
+  username: dbConfig.USER,
+  password: dbConfig.PASSWORD,
+  database: dbConfig.DB,
+  dialect: "postgres",
+  dialectOptions: dbConfig.dialectOptions,
+  port: 5432
+});
 
 module.exports = sequelize;
 
