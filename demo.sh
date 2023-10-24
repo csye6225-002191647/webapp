5201ae857584dcf156c390fb63638ed376b3df6c
Rohit Chouhan <90641522+ChouhanRohitCr07@users.noreply.github.com>
2023-10-23 20:17:24 -0400

    update demo.sh, removed pg

--- demo.sh
+++ demo.sh
@@ -1,22 +1,8 @@
 #!/bin/bash
 
-echo $PGDATABASE
-echo $PGUSER
-echo $PGPASSWORD
-echo $PGPORT
-echo $PGHOST
-echo $PORT
-
 # Update and upgrade packages
 sudo apt update
 sudo apt upgrade -y
-
-# Install PostgreSQL and related packages
-sudo apt install -y postgresql postgresql-contrib
-
-# Start and enable PostgreSQL service
-sudo systemctl start postgresql
-sudo systemctl enable postgresql
 
 # Install Node.js and npm
 sudo apt install -y nodejs
@@ -25,17 +11,17 @@ sudo apt install -y npm
 # Check Node.js version
 nodejs -v
 
-# Configure PostgreSQL: set password, create database, and create user
-sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'postgres';"
-sudo -u postgres psql -c "CREATE USER jarvis WITH PASSWORD 'postgres';"
-sudo -u postgres psql -c "CREATE DATABASE jarvis;"
-sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE "jarvis" to jarvis;"
-
-# unzip and install dependencies
+# unzip and remove artifacts .zip
 sudo apt install unzip
 unzip webapp
 rm -rf webapp.zip
+
+#remove artifacts
 rm -rf artifacts
-sudo apt-get remove -y git
+
+# install dependencies
 cd webapp
 npm install
+
+# Remove git files
+sudo apt-get remove -y git
